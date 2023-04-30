4e91250b9b95acb14b2d6d35e699480a8ebf4702
atomasini <tomasiniarnaldo@gmail.com>
2023-04-30 16:51:18 -0300

    Commit inicial

---
+++ LibreriaHtmlAgilityPack/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net;
+using System.Xml;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // URL de la página web a scrapear
+            string url = "https://www.fravega.com/";
+
+            // Crea un objeto WebClient para descargar el HTML de la página web
+            WebClient webClient = new WebClient();
+            string html = webClient.DownloadString(url);
+            // Crea un objeto HtmlDocument para parsear el HTML
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            // Busca los elementos con la etiqueta "a" y los imprime
+            int i = 0;
+            foreach (HtmlNode link in document.DocumentNode.SelectNodes("//a[@href]"))
+            {
+               // Console.WriteLine(link.Attributes["href"].Value);
+               // i++;
+            }
+            Console.WriteLine(i.ToString());
+
+            // Busca todos los nodos que contienen la palabra "OFERTAS POR TIEMPO LIMITADO"
+            var nodosOfertas = document.DocumentNode.SelectNodes("//*[contains(text(), 'OFERTAS POR TIEMPO LIMITADO')]");
+
+            // Imprime el contenido de cada nodo encontrado
+            foreach (var nodo in nodosOfertas)
+            {
+                Console.WriteLine(nodo.InnerHtml);
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LibreriaHtmlAgilityPack/README.md
@@ -0,0 +1,11 @@
+#  Prueba biblioteca HtmlAgilityPack 
+
+## Que vamos a hacer
+### Comparador de precios de electrodomesticos tomando con entrada inicial la pagina de fravega ofertas limitas 
+### y comparando el producto contra mercadolibre, musimundo,etc.
+
+## Ejemplo de json fravega de ofertas por tiempo limitado
+### Nombre del archivo fravega_ofertas.json
+
+## Ejemplo de json fravega completo
+### Nombre del archivo fravega_web_completa.json
