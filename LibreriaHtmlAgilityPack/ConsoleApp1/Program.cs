afeec84c72bea03729231b05fd63acd98a14499f
atomasini <tomasiniarnaldo@gmail.com>
2023-05-20 21:47:10 -0300

    ML.NET 2

--- LibreriaHtmlAgilityPack/ConsoleApp1/Program.cs
+++ LibreriaHtmlAgilityPack/ConsoleApp1/Program.cs
@@ -34,7 +34,7 @@ namespace WebScraper
             // Imprime el contenido de cada nodo encontrado
             foreach (var nodo in nodosOfertas)
             {
-                nodosOfertas.ToCsv()
+                nodosOfertas.ToC
                 Console.WriteLine(nodo.InnerHtml);
             }
            
