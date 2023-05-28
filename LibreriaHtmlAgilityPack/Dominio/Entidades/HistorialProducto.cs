ceab819ebc73709262a151ab076cd5b810e29340
atomasini <tomasiniarnaldo@gmail.com>
2023-05-28 17:30:40 -0300

    nuevas clases

---
+++ LibreriaHtmlAgilityPack/Dominio/Entidades/HistorialProducto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dominio.Entidades
+{
+    public partial class HistorialProducto
+    {
+        public int? IdProducto { get; set; }
+        public int? IdCoproducto { get; set; }
+
+        public virtual Producto? IdCoproductoNavigation { get; set; }
+        public virtual Producto? IdProductoNavigation { get; set; }
+    }
+}
