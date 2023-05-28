32a720f86def4cd10a00482e76961d906e09993a
Jonathan <jhumanaandia@gmail.com>
2023-05-28 00:10:59 -0300

    Se conectó con EF a traves de project solution de consola

---
+++ LibreriaHtmlAgilityPack/Dominio/Entidades/Historico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dominio.Entidades
+{
+    public partial class Historico
+    {
+        public int Id { get; set; }
+        public int? IdProducto { get; set; }
+        public int? IdCoproducto { get; set; }
+        public int? Puntaje { get; set; }
+    }
+}
